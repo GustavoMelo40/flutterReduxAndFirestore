f637d0e420458fc3324e73b6978a6072f31438bd
Gustavo Melo <gustavomelo40@gmail.com>
2020-05-17 11:30:16 -0300

    apply styles

--- firestore_redux/lib/presentation/todo_item.dart
+++ firestore_redux/lib/presentation/todo_item.dart
@@ -6,6 +6,7 @@ import 'package:fire_redux_sample/models/models.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:todos_app_core/todos_app_core.dart';
+import 'package:circular_check_box/circular_check_box.dart';
 
 class TodoItem extends StatelessWidget {
   final DismissDirectionCallback onDismissed;
@@ -25,30 +26,32 @@ class TodoItem extends StatelessWidget {
     return Dismissible(
       key: ArchSampleKeys.todoItem(todo.id),
       onDismissed: onDismissed,
-      child: ListTile(
-        onTap: onTap,
-        leading: Checkbox(
-          key: ArchSampleKeys.todoItemCheckbox(todo.id),
-          value: todo.complete,
-          onChanged: onCheckboxChanged,
-        ),
-        title: Hero(
-          tag: '${todo.id}__heroTag',
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Text(
-              todo.task,
-              key: ArchSampleKeys.todoItemTask(todo.id),
-              style: Theme.of(context).textTheme.title,
+      child: Container(
+        margin: const EdgeInsets.all(16),
+        decoration: new BoxDecoration(
+            color: Colors.blue,
+            borderRadius: new BorderRadius.all(Radius.circular(8))),
+        child: ListTile(
+          onTap: onTap,
+          leading: CircularCheckBox(
+            key: ArchSampleKeys.todoItemCheckbox(todo.id),
+            value: todo.complete,
+            checkColor: Colors.white,
+            activeColor: Colors.green,
+            inactiveColor: Colors.grey,
+            onChanged: onCheckboxChanged,
+          ),
+          title: Hero(
+            tag: '${todo.id}__heroTag',
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                todo.task,
+                key: ArchSampleKeys.todoItemTask(todo.id),
+                style: Theme.of(context).textTheme.headline6,
+              ),
             ),
           ),
-        ),
-        subtitle: Text(
-          todo.note,
-          key: ArchSampleKeys.todoItemNote(todo.id),
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.subhead,
         ),
       ),
     );
